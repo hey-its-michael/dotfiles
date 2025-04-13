8d244915764e501fd7ed048feadf2ab049fc296c
Michael McCarthy <184942205+hey-its-michael@users.noreply.github.com>
2025-04-13 22:07:49 +0100

    Install brew packages on every apply

--- home/dot_zsh/aliases.zsh
+++ home/dot_zsh/aliases.zsh
@@ -6,3 +6,14 @@ alias grv='git remote -v'
 # Terraform
 alias tf='terraform'
 alias tfp='terraform plan'
+
+# ChezMoi
+alias cm='chezmoi'
+alias cma='chezmoi apply'
+alias cmav='chezmoi apply --verbose'
+alias cmd='chezmoi diff'
+alias cmdv='chezmoi diff --verbose'
+alias cmcd='chezmoi cd'
+alias cmed='chezmoi edit'
+alias cmg='chezmoi git'
+
